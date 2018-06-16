1484b264dcbc0d16a618df6d0f94c4a89c583f96
justbbarnett <justb@inkery.com>
2018-06-16 15:38:46 -0500

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import "../styles/Navbar.css";
+import yoda from '../yoda.png';
 
 export default class Navbar extends Component {
   render() {
@@ -12,7 +13,7 @@ export default class Navbar extends Component {
 
         <div className="mx-auto my-2 order-0 order-md-1 position-relative">
           <a className="mx-auto" href="/">
-            <img src="/assets/images/yoda.png" className="rounded-circle yoda" alt="Logo"/>
+            <img src={yoda} className="rounded-circle yoda" alt="Logo"/>
           </a>
             
         </div>
